ec40fe4443fafad64936068fd49c903e9ddcf177
abdulazizv <abdulazizheir@gmail.com>
2023-02-14 21:39:32 +0500

    branch.controller and branch.service added

--- src/branch/branch.controller.ts
+++ src/branch/branch.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { BranchService } from './branch.service';
+import { createBranchDto } from './dto/create-branch.dto';
+import { updateBranchDto } from './dto/update-branch.dto';
 
 @Controller('api/v1/branch')
 export class BranchController {
-  constructor(private readonly branchService) {
+  constructor(private readonly branchService: BranchService) {}
+
+  @Post('/add')
+  create(@Body() createBranchDto: createBranchDto) {
+    return this.branchService.create(createBranchDto);
+  }
+
+  @Get('/all')
+  getAll() {
+    return this.branchService.getAll();
+  }
+
+  @Get(':id')
+  findOne(@Param() id: string) {
+    return this.branchService.getOne(id);
+  }
+
+  @Put(':id')
+  update(@Param() id: string, @Body() updateBranchDto: updateBranchDto) {
+    return this.branchService.update(id, updateBranchDto);
+  }
+
+  @Delete(':id')
+  remove(@Param() id: string) {
+    return this.branchService.delete(id);
   }
 }
--- src/branch/branch.service.ts
+++ src/branch/branch.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Branch, BranchsDocument } from './schemas/branch.schema';
 import { createBranchDto } from './dto/create-branch.dto';
+import { updateBranchDto } from './dto/update-branch.dto';
 
 @Injectable()
 export class BranchService {
@@ -16,7 +17,9 @@ export class BranchService {
   }
 
   async getAll(): Promise<Branch[]> {
-    const allBranches = await this.branchModule.find();
+    const allBranches = await this.branchModule.find({
+      status: true,
+    });
     if (!allBranches) {
       throw new HttpException('Information not found', HttpStatus.NOT_FOUND);
     }
@@ -34,5 +37,33 @@ export class BranchService {
     return oneBranch;
   }
 
-  async
+  async update(id: string, updateBranchDto: updateBranchDto): Promise<Branch> {
+    const check = await this.branchModule.findById({ _id: id });
+    if (!check) {
+      throw new HttpException(
+        'Id is incorrect, Please give real data !',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const updatedOne = await this.branchModule.findByIdAndUpdate(
+      id,
+      updateBranchDto,
+    );
+    await updatedOne.save();
+    return updatedOne;
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const check = await this.branchModule.findById({ _id: id });
+    if (!check) {
+      throw new HttpException(
+        'ID is incorrect, Please give correct information',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    await this.branchModule.findByIdAndUpdate(id, {
+      status: false,
+    });
+    return true;
+  }
 }
